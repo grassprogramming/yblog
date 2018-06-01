73115d66fd52a2ab0732deba68827245d2ef2632
paul <yanpeng19940119@vip.qq.com>
2018-06-01 17:34:05 +0800

    对封装的打开dialog页面js方法进一步晚上，去除点击空白关闭dialog，点击x关闭dialog的回掉事件去除，去除iframe的framewidth，封装打开提示信息窗口，对对话框的回掉事件嵌套执行进行实际使用测试

--- main/resources/static/common/dialog.js
+++ main/resources/static/common/dialog.js
@@ -9,7 +9,7 @@ function OpenDialog(id,title,url,height,width,callback) {
     });
     dialogInstance.setId(id);
     dialogInstance.setTitle(title);
-    dialogInstance.setMessage($('<div><iframe  src="'+pagepath+'" style="width:100%;height:'+height+' "></iframe></div>'));
+    dialogInstance.setMessage($('<div><iframe  src="'+pagepath+'" style="width:100%;height:'+height+';border-width:0 "></iframe></div>'));
     //打开窗口页面增加接受消息监听，在关闭事件后传递消息通知父级页面关闭dialog
     window.addEventListener('message', receiveMessage, false);
     function receiveMessage(tag) {
@@ -18,12 +18,49 @@ function OpenDialog(id,title,url,height,width,callback) {
             dialogInstance.close();
         }
     }
+    //禁止点击空白关闭窗口
+    dialogInstance.setCloseByBackdrop(false);
     //如果要改变样式，先调用realize方法
     dialogInstance.realize();
+    //原来x关闭按钮也会触发回掉事件，这里把div点击事件重新注册，把dialog的关闭后事件置空
+    dialogInstance.getModalHeader().find("[class='bootstrap-dialog-close-button']").unbind('click').click(function () {
+        dialogInstance.onHidden(function () {
+
+        })
+        dialogInstance.getModal().modal('hide');
+
+    });
+    //打开窗口页面增加接受消息监听，在关闭事件后传递消息通知父级页面关闭dialog
+    window.addEventListener('message', receiveMessage, false);
+    function receiveMessage(tag) {
+        var tag = tag.toString();
+        if(tag='close'){
+            dialogInstance.close();
+        }
+    }
     dialogInstance.getModalBody().css('height',height);
     dialogInstance.getModalBody().css('padding','0');
     dialogInstance.open();
-    BootstrapDialog.close
+}
+
+function OpenMessage() {
+    
+}
+
+function OpenSuccessDialog(callback) {
+    var dialogInstance = new BootstrapDialog({
+        buttons: [{
+            label: '确定',
+            action: function (dialogItself) {
+                dialogItself.close();
+            }
+        }],
+        onhidden:callback
+    });
+    dialogInstance.setTitle('提示信息');
+    dialogInstance.setMessage('操作成功！');
+    dialogInstance.setType(BootstrapDialog.TYPE_SUCCESS);
+    dialogInstance.open();
 }
 
 function CloseDialog() {
